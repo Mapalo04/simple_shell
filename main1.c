61038fbe91edaf5226b697c15c824dcbd7a2ff4f
Mapalo04 <lukundomapalo@gmail.com>
2023-09-22 09:32:28 +0300

    updated main1.c

--- main1.c
+++ main1.c
@@ -6,6 +6,9 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+void execmd(char **args);
+char  **tokenf(char *lineptr, int *n);
+
 /**
  * main - Entry point of the program
  *
@@ -14,12 +17,11 @@
 
 int main(void)
 {
-	char *lineptr, *token;
+	char *lineptr;
 	size_t n = 0;
 	ssize_t nread_ch = 0;
 	char **args = NULL;
-	int i = 0;
-	pid_t pid;
+	int i = 0, a;
 
 	while (1)
 	{
@@ -29,32 +31,63 @@ int main(void)
 		if (nread_ch == -1)
 		{
 			printf("Shell is exiting\n");
-			_exit(1);
+			_exit(0);
 			continue;
 		}
-		args = malloc(sizeof(char *) * 2);
-		token = strtok(lineptr, " \n");
-		i = 0;
-		while (token)
-		{
-			args[i] = strdup(token);
-			i++;
-			token = strtok(NULL, " \n");
-		}
-		args[i] = NULL;
+		args = tokenf(lineptr, &i);
 		
-		pid = fork();
-		if (pid == -1)
-			perror("hsh: fork error");
-		else if (pid == 0)
-		{
-			if (execve(args[0], args, NULL) == -1)
-				perror("hsh: command not found");
-		}
-		else
-			wait(NULL);
-		
+		execmd(args);
+		for (a = 0; a < i; a++)
+			free(args[i]);
+		free(args);
 
 	}
+}
 
+char  **tokenf(char *lineptr, int *n)
+{
+	char **args = NULL, *token, *del = " \n";
+
+	args = malloc(sizeof(char *) * 2);
+	token = strtok(lineptr, " \n");
+	*n = 0;
+	while (token)
+	{
+		args[*n] = strdup(token);
+		*n = *n + 1;
+		token = strtok(NULL, del);
+	}
+	args[*n] = NULL;
+
+	return (args);
+}
+
+/**
+ * execmd - executes commands
+ * @args: list of arguments
+ */
+
+void execmd(char **args)
+{
+	pid_t pid;
+		
+	pid = fork();
+	if (pid == -1)
+	{
+		printf("hsh: fork error");
+		return;
+	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+			perror("hsh: command not found");
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+		return;
+	}
+
+		
 }
